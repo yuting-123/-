8cf42c71e6fe61b6deab3e5ca0f7d4e0869d213c
yuting-123 <126370860+yuting-123@users.noreply.github.com>
2026-02-28 11:44:22 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'carlog-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://unpkg.com/lucide@latest'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
